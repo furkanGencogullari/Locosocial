69e9867d39f2affd884048abd26f19d57a3660af
furkanGencogullari <102527257+furkanGencogullari@users.noreply.github.com>
2022-04-22 16:22:19 +0300

    Created upload screen

--- Locosocial/TableViewCell.swift
+++ Locosocial/TableViewCell.swift
@@ -21,23 +21,19 @@ class TableViewCell: UITableViewCell, MKMapViewDelegate {
     let userInDesc = UILabel()
     let userImage = UIImageView()
     let userNameLabel = UILabel()
+    let scrollView = UIScrollView()
+    var switcher = 0
 
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        card.isHidden = true
+        contentView.backgroundColor = .clear
+        
         let height = contentView.frame.height
         let width = contentView.frame.width
         
-        contentView.backgroundColor = .clear
-        
-        
-        
-        
-        
-        
-        card.isHidden = true
-        
-        card2.frame = CGRect(x: 0, y: 0, width: width * 0.75, height: height * 0.9)
+        card2.frame = CGRect(x: 0, y: 0, width: width * 0.80, height: height * 0.95)
         card2.layer.position = contentView.center
         card2.backgroundColor = .clear
         contentView.addSubview(card2)
@@ -45,18 +41,18 @@ class TableViewCell: UITableViewCell, MKMapViewDelegate {
         map.delegate = self
         map.frame = CGRect(x: card2.frame.width / 2 - (width * 0.70 / 2), y: 10, width: width * 0.70, height: height * 0.45)
         map.layer.cornerRadius = 40
+        
     
         userInDesc.text = "I created amazing memories here. The food is really good and they have fancy wines to go with them."
-        userInDesc.frame = CGRect(x: card2.frame.width / 2 - (width * 0.70 / 2), y: card2.frame.height / 2, width: width * 0.70, height: height * 0.45)
+        userInDesc.textAlignment = .center
+        userInDesc.frame = CGRect(x: card2.frame.width / 2 - (width * 0.70 / 2), y: card2.frame.height / 2 + 3, width: width * 0.70, height: height * 0.25)
         userInDesc.numberOfLines = 5
+        userInDesc.backgroundColor = .clear
         userInDesc.textColor = UIColor(red: (249/255) / 1.5, green: (220/255) / 1.5, blue: 1 / 1.5, alpha: 1)
         userInDesc.font = UIFont(name: "Futura Medium", size: 15)
         
         
-        
-        
-        
-        layer1.frame = CGRect(x: 0, y: 0, width: width * 0.75, height: height * 0.9)
+        layer1.frame = CGRect(x: 0, y: 0, width: width * 0.8, height: height * 0.95)
         layer1.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.5, alpha: 1).cgColor
         layer1.cornerRadius = 40
         layer1.shadowOffset = CGSize(width: 5, height: 5)
@@ -66,12 +62,59 @@ class TableViewCell: UITableViewCell, MKMapViewDelegate {
         layer1.addSublayer(userInDesc.layer)
         card2.addSubview(map)
         
+        scrollView.frame = CGRect(x: card2.frame.width / 2 - 150, y: card2.frame.height * 0.78, width: 300, height: 80)
+        scrollView.layer.cornerRadius = 40
+        scrollView.backgroundColor = UIColor(red: 0.9, green: 1, blue: 1, alpha: 1)
+        scrollView.contentSize = CGSize(width: 380, height: 80)
+        card2.addSubview(scrollView)
         
-        blurView.alpha = 0.6
-        blurView.effect = blurEffect
-        blurView.frame = card2.bounds
-        blurView.layer.cornerRadius = 40
-        blurView.clipsToBounds = true
+        let userImage1 = UIImageView()
+        userImage1.frame = CGRect(x: 10, y: 10, width: 60, height: 60)
+        userImage1.backgroundColor = .clear
+        userImage1.image = UIImage(named: "cafe1")
+        userImage1.layer.cornerRadius = 30
+        userImage1.clipsToBounds = true
+        scrollView.addSubview(userImage1)
+        
+        let userImage2 = UIImageView()
+        userImage2.frame = CGRect(x: 80, y: 10, width: 60, height: 60)
+        userImage2.backgroundColor = .clear
+        userImage2.image = UIImage(named: "cafe2")
+        userImage2.layer.cornerRadius = 30
+        userImage2.clipsToBounds = true
+        scrollView.addSubview(userImage2)
+        
+        let userImage3 = UIImageView()
+        userImage3.frame = CGRect(x: 150, y: 10, width: 60, height: 60)
+        userImage3.backgroundColor = .clear
+        userImage3.image = UIImage(named: "cafe3")
+        userImage3.layer.cornerRadius = 30
+        userImage3.clipsToBounds = true
+        scrollView.addSubview(userImage3)
+        
+        let userImage4 = UIImageView()
+        userImage4.frame = CGRect(x: 220, y: 10, width: 60, height: 60)
+        userImage4.backgroundColor = .clear
+        userImage4.image = UIImage(named: "cafe4")
+        userImage4.layer.cornerRadius = 30
+        userImage4.clipsToBounds = true
+        scrollView.addSubview(userImage4)
+        
+        let userImage5 = UIImageView()
+        userImage5.frame = CGRect(x: 290, y: 10, width: 60, height: 60)
+        userImage5.backgroundColor = .clear
+        userImage5.image = nil
+        userImage5.layer.cornerRadius = 30
+        userImage5.clipsToBounds = true
+        scrollView.addSubview(userImage5)
+        
+        let userImage6 = UIImageView()
+        userImage6.frame = CGRect(x: 360, y: 10, width: 60, height: 60)
+        userImage6.backgroundColor = .clear
+        userImage6.image = nil
+        userImage6.layer.cornerRadius = 30
+        userImage6.clipsToBounds = true
+        scrollView.addSubview(userImage6)
         
         
         
@@ -80,11 +123,19 @@ class TableViewCell: UITableViewCell, MKMapViewDelegate {
         
         
         layer2.frame = CGRect(x: 0, y: 0, width: width * 0.75, height: height * 0.9)
-        layer2.colors = [UIColor(red: 220/255, green: 1, blue: 1, alpha: 0.6).cgColor, UIColor(red: 249/255, green: 220/255, blue: 1, alpha: 0.6).cgColor]
+        layer2.colors = [UIColor(red: 0, green: 1, blue: 173/2, alpha: 0.2).cgColor, UIColor(red: 1, green: 0, blue: 157/255, alpha: 0.2).cgColor]
+        layer2.startPoint = CGPoint(x: 0, y: 0)
+        layer2.endPoint = CGPoint(x: 1, y: 1)
         layer2.cornerRadius = 40
-        // a d d   l a y e r   3
+        
         card2.addSubview(blurView)
         card2.layer.addSublayer(layer2)
+        
+        blurView.alpha = 0.85
+        blurView.effect = blurEffect
+        blurView.frame = layer2.bounds
+        blurView.layer.cornerRadius = 40
+        blurView.clipsToBounds = true
         
         userImage.image = UIImage(named: "zeynep")
         userImage.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
@@ -95,13 +146,13 @@ class TableViewCell: UITableViewCell, MKMapViewDelegate {
         userNameLabel.text = "@zeynepca"
         userNameLabel.frame = CGRect(x: 90, y: 0, width: 180, height: 80)
         userNameLabel.textAlignment = .left
-        userNameLabel.textColor = UIColor(red: (249/255) / 1.5, green: (220/255) / 1.5, blue: 1 / 1.5, alpha: 1)
+        userNameLabel.textColor = UIColor(red: 0.3, green: 0.3, blue: 0.5, alpha: 1)
         userNameLabel.font = UIFont(name: "Futura Medium", size: 20)
         
         
         userDesc.text = "This place is amazing!!"
         userDesc.textAlignment = .center
-        userDesc.textColor = .systemGray
+        userDesc.textColor = .white
         userDesc.frame = CGRect(x: 0, y: 0, width: width * 0.75, height: height * 0.9)
         userDesc.font = UIFont(name: "Futura Medium", size: 20)
         layer2.addSublayer(userDesc.layer)
@@ -109,17 +160,8 @@ class TableViewCell: UITableViewCell, MKMapViewDelegate {
         layer2.addSublayer(userImage.layer)
         
         
-        
-        
-        
-        
-        
-        
-        
-        let gesture = UITapGestureRecognizer(target: self, action: #selector(moveToLeft))
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(fonk))
         card2.addGestureRecognizer(gesture)
-        
-        
         
     }
 
@@ -128,11 +170,21 @@ class TableViewCell: UITableViewCell, MKMapViewDelegate {
 
     }
     
-   @objc func moveToLeft() {
+    
+    @objc func fonk() {
+        if switcher == 0 {
+            moveToLeft()
+        } else if switcher == 1 {
+            goBack()
+        }
+        
+    }
+    
+   func moveToLeft() {
        let animation = CABasicAnimation(keyPath: "position")
        animation.fromValue = CGPoint(x: layer1.frame.origin.x + (layer1.frame.width/2), y: layer1.frame.origin.y + (layer1.frame.height/2))
-       animation.toValue = CGPoint(x: layer1.frame.origin.x - (contentView.frame.width/2), y: layer1.frame.origin.y + (layer1.frame.height/2))
-       animation.duration = 0.5
+       animation.toValue = CGPoint(x: layer1.frame.origin.x - (contentView.frame.width/2) - 80, y: layer1.frame.origin.y + (layer1.frame.height/2))
+       animation.duration = 0.6
        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
        animation.fillMode = .forwards
        animation.isRemovedOnCompletion = false
@@ -140,26 +192,57 @@ class TableViewCell: UITableViewCell, MKMapViewDelegate {
        layer2.add(animation, forKey: nil)
        blurView.layer.add(animation, forKey: nil)
        
-       
        let rotationn = CABasicAnimation(keyPath: "transform.rotation")
        rotationn.fromValue = 0
-       rotationn.toValue = 0.2
-       rotationn.duration = 0.5
+       rotationn.toValue = 0.3
+       rotationn.duration = 0.6
        rotationn.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
        rotationn.fillMode = .forwards
        rotationn.isRemovedOnCompletion = false
        rotationn.beginTime = 0
+       blurView.layer.add(rotationn, forKey: nil)
        layer2.add(rotationn, forKey: nil)
        
+       Timer.scheduledTimer(withTimeInterval: 0.6, repeats: false) { (_) in
+           self.layer2.isHidden = true
+           self.blurView.isHidden = true
+       }
        
-        
+       switcher = 1
     }
     
-    @objc func rotate() {
+    func goBack() {
+        self.layer2.isHidden = false
+        self.blurView.isHidden = false
+        
+        
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.fromValue = CGPoint(x: layer1.frame.origin.x - (contentView.frame.width/2) - 80, y: layer1.frame.origin.y + (layer1.frame.height/2))
+        animation.toValue = CGPoint(x: layer1.frame.origin.x + (layer1.frame.width/2), y: layer1.frame.origin.y + (layer1.frame.height/2))
+        animation.duration = 0.6
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.beginTime = 0
+        layer2.add(animation, forKey: nil)
+        blurView.layer.add(animation, forKey: nil)
         
         
         
-         
+        
+        let rotationn = CABasicAnimation(keyPath: "transform.rotation")
+        rotationn.fromValue = 0.3
+        rotationn.toValue = 0
+        rotationn.duration = 0.75
+        rotationn.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        rotationn.fillMode = .forwards
+        rotationn.isRemovedOnCompletion = false
+        rotationn.beginTime = 0
+        blurView.layer.add(rotationn, forKey: nil)
+        layer2.add(rotationn, forKey: nil)
+        
+        switcher = 0
      }
+    
 
 }
---
+++ Locosocial/UploadViewController.swift
@@ -0,0 +1,218 @@
+//
+//  UploadViewController.swift
+//  Locosocial
+//
+//  Created by Furkan Gençoğulları on 22.04.2022.
+//
+
+import UIKit
+import MapKit
+
+class UploadViewController: UIViewController, MKMapViewDelegate, UITextViewDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let scrollView = UIScrollView()
+    let mapView = MKMapView()
+    let titleField = UITextField()
+    let descField = UITextView()
+    let imageScroll = UIScrollView()
+    let addImageButton = UIButton()
+    let imagePlaceholder = UILabel()
+    let uploadButton = UIButton()
+    let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.extraLight)
+    
+    let imageView1 = UIImageView()
+    let imageView2 = UIImageView()
+    let imageView3 = UIImageView()
+    let imageView4 = UIImageView()
+    let imageView5 = UIImageView()
+    let imageView6 = UIImageView()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.5, alpha: 1)
+        scrollView.contentSize = CGSize(width: view.frame.width, height: 1000)
+        view.addSubview(scrollView)
+        
+        
+        mapView.frame = CGRect(x: view.frame.width / 2 - 190, y: 0, width: 380, height: 400)
+        mapView.delegate = self
+        mapView.layer.cornerRadius = 40
+        scrollView.addSubview(mapView)
+        
+        
+        titleField.frame = CGRect(x: view.frame.width / 2 - 190, y: 415, width: 380, height: 80)
+        titleField.layer.cornerRadius = 40
+        titleField.backgroundColor = .clear
+        titleField.textAlignment = .center
+        titleField.font = UIFont(name: "Futura Medium", size: 20)
+        titleField.placeholder = "Title"
+        createBackground(Frame: titleField.frame, View: titleField)
+        scrollView.addSubview(titleField)
+        
+        
+        descField.delegate = self
+        descField.frame = CGRect(x: view.frame.width / 2 - 190, y: 510, width: 380, height: 300)
+        descField.text = "Description"
+        descField.backgroundColor = .clear
+        descField.textColor = .lightGray
+        descField.layer.cornerRadius = 40
+        descField.textAlignment = .center
+        descField.font = UIFont(name: "Futura Medium", size: 17)
+        createBackground(Frame: descField.frame, View: descField)
+        scrollView.addSubview(descField)
+        
+        
+        imageScroll.frame = CGRect(x: view.frame.width / 2 - 190, y: 825, width: 380, height: 80)
+        imageScroll.backgroundColor = .clear
+        imageScroll.contentSize = CGSize(width: 500, height: 80)
+        imageScroll.layer.cornerRadius = 40
+        createBackground(Frame: imageScroll.frame, View: imageScroll)
+        scrollView.addSubview(imageScroll)
+        
+        
+        imagePlaceholder.frame = CGRect(x: 40, y: 10, width: 360, height: 60)
+        imagePlaceholder.textColor = UIColor.lightGray
+        imagePlaceholder.text = "Add Images"
+        imagePlaceholder.font = UIFont(name: "Futura Medium", size: 17)
+        imageScroll.addSubview(imagePlaceholder)
+        
+        
+        addImageButton.frame = CGRect(x: view.frame.width / 2 - 190 + 300, y: 825, width: 80, height: 80)
+        addImageButton.tintColor = .blue
+        addImageButton.configuration = .filled()
+        addImageButton.layer.cornerRadius = 40
+        addImageButton.clipsToBounds = true
+        addImageButton.backgroundColor = .darkGray
+        addImageButton.setImage(UIImage(systemName: "plus.circle.fill"), for: UIControl.State.normal)
+        addImageButton.titleLabel?.text = nil
+        addImageButton.addTarget(self, action: #selector(addImageButtonPressed), for: UIControl.Event.touchDown)
+        scrollView.addSubview(addImageButton)
+        
+        
+        
+        imageView1.frame = CGRect(x: 10, y: 10, width: 60, height: 60)
+        imageView1.backgroundColor = .clear
+        imageView1.image = nil
+        imageView1.layer.cornerRadius = 30
+        imageView1.clipsToBounds = true
+        imageScroll.addSubview(imageView1)
+        
+        imageView2.frame = CGRect(x: 80, y: 10, width: 60, height: 60)
+        imageView2.backgroundColor = .clear
+        imageView2.image = nil
+        imageView2.layer.cornerRadius = 30
+        imageView2.clipsToBounds = true
+        imageScroll.addSubview(imageView2)
+        
+        imageView3.frame = CGRect(x: 150, y: 10, width: 60, height: 60)
+        imageView3.backgroundColor = .clear
+        imageView3.image = nil
+        imageView3.layer.cornerRadius = 30
+        imageView3.clipsToBounds = true
+        imageScroll.addSubview(imageView3)
+        
+        imageView4.frame = CGRect(x: 220, y: 10, width: 60, height: 60)
+        imageView4.backgroundColor = .clear
+        imageView4.image = nil
+        imageView4.layer.cornerRadius = 30
+        imageView4.clipsToBounds = true
+        imageScroll.addSubview(imageView4)
+        
+        imageView5.frame = CGRect(x: 290, y: 10, width: 60, height: 60)
+        imageView5.backgroundColor = .clear
+        imageView5.image = nil
+        imageView5.layer.cornerRadius = 30
+        imageView5.clipsToBounds = true
+        imageScroll.addSubview(imageView5)
+        
+        imageView6.frame = CGRect(x: 360, y: 10, width: 60, height: 60)
+        imageView6.backgroundColor = .clear
+        imageView6.image = nil
+        imageView6.layer.cornerRadius = 30
+        imageView6.clipsToBounds = true
+        imageScroll.addSubview(imageView6)
+        
+        
+        
+        uploadButton.frame = CGRect(x: view.frame.width / 2 - 190, y: 920, width: 380, height: 80)
+        uploadButton.titleLabel?.font = UIFont(name: "Futura Medium", size: 20)
+        uploadButton.setTitle("Upload", for: UIControl.State.normal)
+        uploadButton.layer.cornerRadius = 40
+        uploadButton.clipsToBounds = true
+        uploadButton.addTarget(self, action: #selector(uploadButtonPressed), for: UIControl.Event.touchDown)
+        scrollView.addSubview(uploadButton)
+
+    }
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if descField.textColor == .lightGray {
+            descField.textColor = .black
+            descField.text =  ""
+        }
+    }
+    
+    
+    @objc func addImageButtonPressed() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if imageView1.image == nil {
+            imageView1.image = info[.originalImage] as? UIImage
+            imagePlaceholder.isHidden = true
+        } else if imageView2.image == nil {
+            imageView2.image = info[.originalImage] as? UIImage
+        } else if imageView3.image == nil {
+            imageView3.image = info[.originalImage] as? UIImage
+        } else if imageView4.image == nil {
+            imageView4.image = info[.originalImage] as? UIImage
+        } else if imageView5.image == nil {
+            imageView5.image = info[.originalImage] as? UIImage
+        } else if imageView6.image == nil {
+            imageView6.image = info[.originalImage] as? UIImage
+        } else {
+            let alert = UIAlertController(title: "Error", message: "You can add six images", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
+            alert.addAction(okButton)
+        }
+        dismiss(animated: true)
+    }
+    
+    
+    @objc func uploadButtonPressed() {
+        print("upload clicked")
+        
+        
+        
+    }
+    
+    
+    func createBackground (Frame: CGRect, View: UIView) {
+        let layer2 = CAGradientLayer()
+        let blurView = UIVisualEffectView()
+        
+        blurView.alpha = 0.85
+        blurView.effect = blurEffect
+        blurView.frame = Frame
+        blurView.layer.cornerRadius = 40
+        blurView.clipsToBounds = true
+        scrollView.addSubview(blurView)
+        
+        
+        layer2.frame = Frame
+        layer2.colors = [UIColor(red: 0, green: 1, blue: 173/2, alpha: 0.2).cgColor, UIColor(red: 1, green: 0, blue: 157/255, alpha: 0.2).cgColor]
+        layer2.startPoint = CGPoint(x: 0, y: 0)
+        layer2.endPoint = CGPoint(x: 1, y: 1)
+        layer2.cornerRadius = 40
+        scrollView.layer.addSublayer(layer2)
+    }
+   
+    
+
+
+}
